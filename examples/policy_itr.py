9010f990d0f606dd524bc186d96f2f4874891fde
ilavie88 <itai.lavie@campus.technion.ac.il>
2024-04-24 10:03:41 +0300

    Changes I made in orignal repo so that PI will run

--- examples/policy_itr.py
+++ examples/policy_itr.py
@@ -3,6 +3,13 @@ from library.gridenv import small_env_fn
 import matplotlib.pyplot as plt
 from library.helper import inf_norm,getMRP
 from PIL import Image
+import os
+
+# Added code to make sure file exists
+# Construct the full file path using SCRIPT_PATH
+script_path = os.environ.get("SCRIPT_PATH")
+
+
 
 np.random.seed(42)
 env=small_env_fn(42)
@@ -22,14 +29,15 @@ while np.sum(np.abs(pi-pi_prev))>0: #until no policy change
     V=R_s+gamma*np.matmul(P_ss,V)
     pi=np.argmax(env.R_sa+gamma*np.squeeze(np.matmul(env.P_sas,V)),axis=1)
     image=Image.fromarray(env.getScreenshot(pi))
-    image.save(f"./logs/policy_itr/pi_{i}.png")
+    # image.save(f"./logs/policy_itr/pi_{i}.png") # Changed due to some problem in saving the img
+    image.save(os.path.join(script_path, 'logs', 'policy_itr', f"pi_{i}.png")) # ilavie - new codeline
     v_values.append(inf_norm(V))
     i+=1
 
 report=f"Converged in {i} iterations\n"
 report+=f"Pi_*= {pi}\n"
 report+=f"V_*= {V.flatten()}\n"
-with open("./logs/policy_itr/report.txt","w") as f:f.write(report)
+with open(os.path.join(script_path, "logs", "policy_itr","report.txt"), "w") as f:f.write(report)
 print(report)
 
 plt.plot(v_values,lw=3,ls='--')
@@ -37,4 +45,6 @@ plt.ylabel('$|V|_{\infty}$',fontsize=16)
 plt.xticks(range(len(v_values)),labels=["$\pi_{"+f"{e}"+"}$" for e in range(len(v_values))])
 plt.xlabel('Policy',fontsize=16)
 plt.tight_layout()
-plt.savefig("./logs/policy_itr/pi_itr_v.png")
+
+# plt.savefig("./logs/policy_itr/pi_itr_v.png")
+plt.savefig(os.path.join(script_path, "logs", "policy_itr","pi_itr_v.png")) # ilavie - changed
